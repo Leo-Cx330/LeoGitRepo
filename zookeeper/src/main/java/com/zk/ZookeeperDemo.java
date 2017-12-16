5f04ec075a3e574b262a5dad7cc49cb7ad4a7d8f
lihao <lhwyemail@163.com>
2017-12-16 15:05:37 +0800

    。。。。。

--- zookeeper/src/main/java/com/zk/ZookeeperDemo.java
+++ zookeeper/src/main/java/com/zk/ZookeeperDemo.java
@@ -13,37 +13,34 @@ import org.apache.zookeeper.ZooKeeper;
 
 @SuppressWarnings("unused")
 public class ZookeeperDemo {
-	
+
 	private static final String CONNECT_ADDR = "192.168.75.132:2181,192.168.75.133:2181,192.168.75.136:2181";
-	
+
 	private static final int SESSION_OUTTIME = 2000;
-	
+
 	private static final CountDownLatch COUNT_DOWN_LATCH=new CountDownLatch(1);
-	
+
 	public static void main(String[] args) throws Exception {
 		ZooKeeper zk=new ZooKeeper(CONNECT_ADDR, SESSION_OUTTIME, new Watcher() {
-			
+
 			public void process(WatchedEvent event) {
 				KeeperState state = event.getState();
 				EventType type = event.getType();
-				//�����������
+				//如果建立连接
 				if(KeeperState.SyncConnected==state){
 					if(EventType.None==type){
 						COUNT_DOWN_LATCH.countDown();
-						System.out.println("zk ��������");
+						System.out.println("zk 建立连接");
 					}
 				}
-				
+
 			}
 		});
 		COUNT_DOWN_LATCH.await();
-		
+
 		String result = zk.create("/hh/jj", "hello".getBytes(),
 				Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 		System.out.println("result:"+result);
 		zk.close();
-
-
-		 System.out.println("result:");
 	}
 }
