f738ff9c0dd85cc88cf220b04077a9ab364792f1
li.hao@puscene.con <li.hao@puscene.con@puscene.com>
2018-03-15 18:34:21 +0800

    add config

--- Leo-cloud/cloud-config-server/src/main/java/com/leo/app/CloudConfigServer.java
+++ Leo-cloud/cloud-config-server/src/main/java/com/leo/app/CloudConfigServer.java
@@ -1,15 +1,9 @@
 package com.leo.app;
 
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-import java.util.concurrent.LinkedBlockingQueue;
-
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
